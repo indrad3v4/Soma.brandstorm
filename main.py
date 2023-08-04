5953963caa3052d3ccb074dc1e876229f8cd4574
indrad3v4 <120383053+indrad3v4@users.noreply.github.com>
2023-08-04 16:16:34 +0000

    Summary: "Updated AI model to include conversation history"
    Description: "Modified the application to store the conversation history in the user's session. This history is now included in the prompt for the AI model, allowing it to generate responses that take into account the broader context of the conversation. The session is cleared after generating the final result."

--- main.py
+++ main.py
@@ -24,48 +24,28 @@ questions = [
     "Enter your timeframe:",
 ]
 
-question_temperatures = [
-    # Define a custom temperature for each question, for example:
-    0.3, 0.5, 0.6, 0.5, 0.4, 0.3, 0.5, 0.4, 0.3, 0.5, 0.6, 0.4, 0.3, 0.5, 0.4, 0.3
-]
-
-# Initialize an empty list to store the questions and answers
-conversation_history = []
-
-
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if 'result' not in session:
-        session['result'] = ""  # Initialize the result in the session
+    if 'conversation_history' not in session:
+        session['conversation_history'] = []
 
     question_index = int(request.form.get('question_index', 0))
     user_input = request.form.get('user_input', "")
 
     if request.method == 'POST':
-        # The user wants an AI-generated answer
-        api_key = os.getenv('API_KEY')
-        openai.api_key = api_key
-
-        # Add the current question and user input to the conversation history
-        conversation_history.append(questions[question_index])
-        conversation_history.append(user_input)
-
-        # Generate the AI's response using the entire conversation history
-        response = openai.Completion.create(
-            engine="davinci", 
-            prompt="\n".join(conversation_history), 
-            max_tokens=60
-        )
-        user_input = response.choices[0].text.strip()
-
-        session['result'] += f"{questions[question_index]} {user_input}\n"  # Store the result in the session
+        if user_input == "":
+            api_key = os.getenv('API_KEY')
+            openai.api_key = api_key
+            prompt = '\n'.join(session['conversation_history'] + [questions[question_index]])
+            response = openai.Completion.create(engine="davinci", prompt=prompt, max_tokens=60)
+            user_input = response.choices[0].text.strip()
+        
+        session['conversation_history'] += [questions[question_index], user_input]
         question_index += 1
 
     if question_index >= len(questions):
-        # Construct the final prompt
-        answers = result.split('\n')[:-1]  # Split the result into a list of answers
-        answers = [answer.split(': ')[1] for answer in answers]  # Remove the question part from each answer
+        session.clear()
+        answers = session['conversation_history'][1::2]
         final_prompt = f"""
         I, {answers[0]}, am on a journey from {answers[1]} to {answers[2]}. 
         My brand, {answers[3]}, is guided by our core values of {answers[4]}, and we envision a future where {answers[5]}.
@@ -76,14 +56,13 @@ def index():
         With a budget of {answers[14]}, my goal is to achieve {answers[1]} by {answers[15]}.
         """
 
-        # Generate the brand book
         api_key = os.getenv('API_KEY')
         openai.api_key = api_key
         response = openai.Completion.create(engine="davinci", prompt=final_prompt, max_tokens=500, temperature=0.5, top_p=1)
         result = response.choices[0].text.strip()
-        return render_template('result.html', result=result)  # Render a different template to show the final result
+        return render_template('result.html', result=result)
 
-    return render_template('index.html', result=session['result'], question=questions[question_index], question_index=question_index)
+    return render_template('index.html', question=questions[question_index], question_index=question_index)
 
 if __name__ == "__main__":
     app.run(debug=True)
